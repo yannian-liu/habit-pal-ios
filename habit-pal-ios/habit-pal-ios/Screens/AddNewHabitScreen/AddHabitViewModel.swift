f5d3ae4fa8110bfb9b720dbc619316836de58b5f
Yannian Liu <yannian.liu@outlook.com>
2024-04-03 17:58:11 +1100

    Update emoji button

--- habit-pal-ios/habit-pal-ios/Screens/AddNewHabitScreen/AddHabitViewModel.swift
+++ habit-pal-ios/habit-pal-ios/Screens/AddNewHabitScreen/AddHabitViewModel.swift
@@ -11,13 +11,16 @@ import Combine
 import SwiftUI
 
 class AddHabitViewModel: ObservableObject {
+    typealias EmojiButton = StatableButtonViewConfiguration<Text>
+    private var cancellables = Set<AnyCancellable>()
+
     @ObservedObject private var habitsViewModel: HabitsViewModel
 
     public lazy var navigationConfiguration = NavigationViewConfiguration(title: "Add a Habit") { [unowned self] in
             habitsViewModel.shouldShowAddHabitView = false
     }
     
-    public lazy var emojiTextField = OneCharTextFieldViewConfiguration(
+    public var emojiTextField = OneCharTextFieldViewConfiguration(
         contentDisplay: .emoji,
         plateDisplay: .emojiForDetail(plateColor: .red),
         accentColor: .clear,
@@ -27,20 +30,31 @@ class AddHabitViewModel: ObservableObject {
         tag: 0
     )
         
-    lazy var emojiButton = StatableButtonViewConfiguration(
-        content: Text("💧"),
-        activeDisplay: .emojiForDetail(plateColor: .blue),
-        disabledDisplay: nil,
-        highlightedDisplay: nil,
-        animation: .scale,
-        statePublisher: nil,
-        action: { [unowned self] in
-            emojiTextField.textFieldViewConfiguration.becomeFocus()
-        }
-    )
+    @Published var emojiButton: EmojiButton!
 
     init(habitsViewModel: HabitsViewModel) {
         self.habitsViewModel = habitsViewModel
-        
+    
+        emojiTextField.textFieldViewConfiguration.$text
+            .dropFirst()
+            .prepend("💧")
+            .map { [unowned self] in
+                makeEmojiButton(with: $0)
+            }
+            .assign(to: &$emojiButton)
+    }
+
+    private func makeEmojiButton(with text: String) -> EmojiButton {
+        StatableButtonViewConfiguration(
+            content: Text(text),
+            activeDisplay: .emojiForDetail(plateColor: .blue),
+            disabledDisplay: nil,
+            highlightedDisplay: nil,
+            animation: .scale,
+            statePublisher: nil,
+            action: { [unowned self] in
+                emojiTextField.textFieldViewConfiguration.becomeFocus()
+            }
+        )
     }
 }
